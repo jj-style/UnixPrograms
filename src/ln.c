f5300bc1e49d8ae13c3f5e216073e78f539b8e02
JJ Style <style.jj@pm.me>
2020-06-03 09:07:37 +0100

    use realpath and get symlink right way round

--- src/ln.c
+++ src/ln.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <linux/limits.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
 
 int sflag = 0;
@@ -31,22 +30,10 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    for(i=0;i<2;++i) {
-        /* remove trailing / in source and destination if there is one */
-        if (argv[i][strlen(argv[i])-1] == '/')
-            argv[i][strlen(argv[i])-1] = '\0';
-        
-        /* build absolute path if not given */
-        if (argv[i][0] == '/') {
-            strncpy(files[i],argv[i],PATH_MAX);
-        } else {
-            getcwd(files[i],PATH_MAX);
-            strcat(files[i],"/");
-            strcat(files[i],argv[i]);
-        }
-    }
+    for(i=0;i<2;++i)
+        realpath(argv[i],files[i]); /* build absolute path if not given for the arguments */
 
-    sflag ? (res = link(files[0],files[1])) : (res = symlink(files[0],files[1]));
+    sflag ? (res = symlink(files[0],files[1])) : (res = link(files[0],files[1]));
     if ( res == -1 ) {
         perror("link error");
     }
